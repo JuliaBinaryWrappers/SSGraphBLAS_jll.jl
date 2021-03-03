9f7fee421b9a27a7eeafd729b882d71929f81bf2
jlbuild <juliabuildbot@gmail.com>
2021-03-03 22:36:37 +0000

    SSGraphBLAS_jll build 4.0.3+0

---
+++ src/SSGraphBLAS_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule SSGraphBLAS_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("SSGraphBLAS")
+JLLWrappers.@generate_main_file("SSGraphBLAS", UUID("7ed9a814-9cab-54e9-8e9e-d9e95b4d61b1"))
+end  # module SSGraphBLAS_jll
