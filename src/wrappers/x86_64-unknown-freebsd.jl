5e0bf2a2f10d28d8de3e33705287bc49067eb943
jlbuild <juliabuildbot@gmail.com>
2021-05-14 21:53:45 +0000

    SSGraphBLAS_jll build 5.0.4+0

--- src/wrappers/x86_64-unknown-freebsd.jl
+++ src/wrappers/x86_64-unknown-freebsd.jl
@@ -3,7 +3,7 @@ export libgraphblas
 
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("SSGraphBLAS")
-JLLWrappers.@declare_library_product(libgraphblas, "libgraphblas.so.4")
+JLLWrappers.@declare_library_product(libgraphblas, "libgraphblas.so.5")
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
