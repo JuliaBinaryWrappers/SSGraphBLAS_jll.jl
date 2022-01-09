a4302e7e12c6dea7941a6880160e448276e26923
jlbuild <juliabuildbot@gmail.com>
2022-01-09 16:26:02 +0000

    SSGraphBLAS_jll build 6.1.3+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libgraphblas,
-        "lib/libgraphblas.6.0.0.dylib",
+        "lib/libgraphblas.6.1.3.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
