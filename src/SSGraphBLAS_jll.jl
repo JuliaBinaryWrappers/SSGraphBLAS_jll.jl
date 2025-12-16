f4c68f37bc8a5ac3d6f782ed39fbdaf1d80a5b5d
jlbuild <juliabuildbot@gmail.com>
2025-12-16 22:19:06 +0000

    SSGraphBLAS_jll build 10.2.0+0

--- src/SSGraphBLAS_jll.jl
+++ src/SSGraphBLAS_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SSGraphBLAS")
-JLLWrappers.@generate_main_file("SSGraphBLAS", UUID("7ed9a814-9cab-54e9-8e9e-d9e95b4d61b1"))
+JLLWrappers.@generate_main_file("SSGraphBLAS", Base.UUID("7ed9a814-9cab-54e9-8e9e-d9e95b4d61b1"))
 end  # module SSGraphBLAS_jll
